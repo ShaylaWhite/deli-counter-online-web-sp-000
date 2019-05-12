3e1f20d5fe24ec6249ca4db6a75982c94da3e49c
Shayla White <shayladwhite1@gmail.com>
2019-05-12 03:53:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,6 +17,7 @@ def take_a_number(katz_deli,person)
   if katz_deli.length == 0
     katz_deli.push(person)
   puts "Welcome, #{person}. You are number #{katz_deli.length} in line."
+elsif
 end
  end
  def now_serving(katz_deli)
