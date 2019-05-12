a2f19a2dc20a73ec24d30b251984b81d81f6d2a2
Shayla White <shayladwhite1@gmail.com>
2019-05-12 03:38:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,5 +7,6 @@ end
 
 def take_a_number(katz_deli,name)
   katz_deli.each do |name|
-    puts "Welcome, #{name}. You are number  in line."
+    puts "Welcome, #{name}. You are #{number}  in line."
+    line 
 end
