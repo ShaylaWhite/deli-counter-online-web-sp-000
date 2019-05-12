621a69cbe91220dae3689b0f5d1fff9d0abebcd7
Shayla White <shayladwhite1@gmail.com>
2019-05-12 03:40:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,14 @@
 def line(katz_deli)
- number_list = []
+  numbered_list = []
   number = 1
-end
+  if katz_deli.length == 0
+    puts "The line is currently empty."
 
+  else
+    katz_deli.each do |name|
+      numbered_list.push("#{number}. #{name}")
+      number += 1
+    end
+    puts "The line is currently: #{numbered_list.join(" ")}"
+  end
+end
